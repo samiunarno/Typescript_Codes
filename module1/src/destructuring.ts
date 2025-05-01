e1d94f9eedc98d6aa36a91ec4c2f9fb2717c6e5f
samiunarno <samiunarno.iub@gmail.com>
2025-05-02 00:49:06 +0600

    new ts

---
+++ module1/src/destructuring.ts
@@ -0,0 +1,23 @@
+const user = {
+  id: 101,
+  name: {
+    first: "Samiun",
+    last: "Arno", // once only
+  },
+  cont: "017000888990",
+  address: "uganda",
+};
+
+const {
+  name: { last },
+  cont: contactNo,
+} = user;
+
+console.log(last);
+console.log(contactNo);
+
+const myArray = ["chanler", "Joe", "redchil"];
+
+const [, , best, ...rest] = myArray;
+
+console.log(myArray);
--- module1/src/new1.ts
+++ module1/src/new1.ts
@@ -1,16 +1,18 @@
-const user: {
-  company: string;
-  first: string;
-  last: string;
-  age: number;
-  isActive: boolean;
-} = {
-  company: "NewLab",
-  first: "Samiun",
-  last: "Mahmud",
-  age: 23,
-  isActive: true,
-};
+{
+  const user: {
+    company: string;
+    first: string;
+    last: string;
+    age: number;
+    isActive: boolean;
+  } = {
+    company: "NewLab",
+    first: "Samiun",
+    last: "Mahmud",
+    age: 23,
+    isActive: true,
+  };
 
-user.company = "Newlab INC";
-console.log(user);
+  user.company = "Newlab INC";
+  console.log(user);
+}
